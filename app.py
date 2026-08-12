c1b21443f5a56724e22ca1267c63fc7a6d0f5a86
KeyZJahat <arifhaikal0309@gmail.com>
2026-08-12 14:58:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,80 +1,53 @@
-import streamlit as st
+"""A script to check whether a variable `_RELEASE` is set as True."""
 
-st.set_page_config(
-    page_title="streamlit-folium documentation",
-    page_icon=":world_map:️",
-    layout="wide",
-)
-
-"# streamlit-folium"
-
-"""streamlit-folium integrates two great open-source projects in the Python ecosystem:
-[Streamlit](https://streamlit.io) and
-[Folium](https://python-visualization.github.io/folium/)!"""
-
-"""
-Currently, there are two functions defined:
-
-- `st_folium()`: a bi-directional Component, taking a Folium/Branca object and plotting
-  to the Streamlit app. Upon mount/interaction with the Streamlit app, st_folium()
-  returns a Dict with selected information including the bounding box and items clicked
-  on
-
-- `folium_static()`: takes a folium.Map, folium.Figure, or branca.element.Figure object
-  and displays it in a Streamlit app.
-"""
-
-"""
-On its own, Folium is limited to _display-only_ visualizations; the Folium API generates
-the proper [leaflet.js](https://leafletjs.com/) specification, as HTML and displays it.
-Some interactivity is provided (depending on how the Folium API is utilized), but the
-biggest drawback is that the interactivity from the visualization isn't passed back to
-Python, and as such, you can't make full use of the functionality provided by the
-leaflet.js library.
-
-`streamlit-folium` builds upon the convenient [Folium
-API](https://python-visualization.github.io/folium/modules.html) for building geospatial
-visualizations by adding a _bi-directional_ data transfer functionality. This not only
-allows for increased interactivity between the web browser and Python, but also the use
-of larger datasets through intelligent querying.
-
-### Bi-directional data model
-"""
-left, right = st.columns(2)
+import argparse
+import ast
+import sys
+from pathlib import Path
+from typing import cast
 
 
-with left:
-    """
-    If we take a look at the example from the Home page, it might seem trivial. We
-    define a single point with a marker and pop-up and display it:
-    """
-    with st.echo():
-        import folium
-        import streamlit as st
+def get_release_flag_value(filepath: Path):
+    with open(filepath) as f:
+        fbody = f.read()
 
-        from streamlit_folium import st_folium
+    parsed_ast = ast.parse(fbody)
 
-        # center on Liberty Bell, add marker
-        m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
-        folium.Marker(
-            [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
-        ).add_to(m)
+    toplevel_assignments = [
+        node for node in parsed_ast.body if isinstance(node, ast.Assign)
+    ]
 
-        # call to render Folium map in Streamlit
-        st_data = st_folium(m, width=725)
+    release_val = None
+    for node in toplevel_assignments:
+        if len(node.targets) != 1:
+            continue
+        if not isinstance(node.targets[0], ast.Name):
+            continue
+        single_target = cast(ast.Name, node.targets[0])
 
-with right:
-    """
-    But behind the scenes, a lot more is happening _by default_. The return value of
-    `st_folium` is set to `st_data`, and within this Python variable is information
-    about what is being displayed on the screen:
-    """
+        assigned_value = node.value
 
-    st_data
+        if single_target.id == "_RELEASE":
+            if not isinstance(assigned_value, ast.Constant):
+                raise Exception(
+                    f"Not a constant value {assigned_value} is "
+                    f"assigned to {single_target}"
+                )
+            release_val = assigned_value.value
 
-    """
-    As the user interacts with the data visualization, the values for `bounds` are
-    constantly updating, along with `zoom`. With these values available in Python, we
-    can now limit queries based on bounding box, change the marker size based on the
-    `zoom` value and much more!
-    """
+    return release_val
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", type=Path)
+
+    args = parser.parse_args()
+
+    is_release = get_release_flag_value(args.filename)
+
+    if not is_release:
+        print("_RELEASE flag is not set as True")
+        sys.exit(-1)
+
+    sys.exit(0)
